53a288fb04e9cb8c144ae148c9784630c33ec97c
Alfred Lieth Årøe <alfredla@Alfreds-MBP.lan>
2018-06-11 22:24:58 +0200

    Removed unnecessary functions

--- Day 13/day13b.py
+++ Day 13/day13b.py
@@ -16,26 +16,8 @@ def readInput():
 		last += 1
 	return l
 
-# returns True if you are chaught, False if you are not
-def chaughtByScanner(scanner, pos):
-	if not scanner[pos][0] == 0:
-		if scanner[pos][1] == 0:
-			return True
-	return False
-
 def getScannerPosition(s, delay):
 	return (delay + s[0]) % (2*s[1]-2)
-
-
-
-def moveOneScanner(s):
-	if not s[0] == 0:
-		if s[2]: 	s[1] += 1
-		else: 		s[1] -= 1
-
-		if s[1] == 0 or s[1] == s[0]-1:
-			s[2] = not s[2]
-
 
 scanner = readInput()
 chaught = True
