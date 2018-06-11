4bf1bcb993a29798f9078d7d43fa24edc18801a5
Alfred Lieth Årøe <alfredla@Alfreds-MBP.lan>
2018-06-11 21:52:16 +0200

    Day 8 done, learned about lambda and dict in python

---
+++ Day 8/day8a.py
@@ -0,0 +1,43 @@
+# read the puzzle
+puzzle = open('input.txt', 'r')
+
+# define the operators
+operator = {	
+	'>=':  (lambda x,y: x>=y), 
+	'<=':  (lambda x,y: x<=y),
+	'>' :  (lambda x,y: x>y ),
+	'<' :  (lambda x,y: x<y ),
+	'!=':  (lambda x,y: x!=y),
+	'==':  (lambda x,y: x==y),
+	'dec': (lambda x,y: x-y ),
+	'inc': (lambda x,y: x+y )
+}
+
+registers = {}
+maxValue = 0
+
+# loop through each line in puzzle input
+for line in puzzle:
+	parts = line.split(' ')
+
+	# initialize the registers if they are already
+	if not parts[0] in registers:
+		registers[parts[0]] = 0
+	if not parts[4] in registers:
+		registers[parts[4]] = 0
+
+	#  0   1   2  3   4  5  6
+	# gug dec 188 if zpw >= 8
+
+	# check condition
+	if operator[parts[5]](registers[parts[4]], int(parts[6])):
+
+		# update set if needed
+		registers[parts[0]] = operator[parts[1]](registers[parts[0]], int(parts[2]))
+
+
+print(max(registers.values()))
+
+
+
+
